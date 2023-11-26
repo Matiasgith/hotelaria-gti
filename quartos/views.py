276cc883fcbb1f6f50fcd8ae6870eb23ab18b4e2
unknown <matiassouza995@gmail.com>
2023-11-26 18:08:56 -0300

    listagem feita de quarto, pagina de detalhes de quartos feita tanbem e adicionei mais um stilo no style_Photelaria.css

--- quartos/views.py
+++ quartos/views.py
@@ -4,10 +4,11 @@ from django.db.models import Q
 from reservas.models import Quartos
 
 def quartos(request):
-
-    return render(request, 'quartos/quartos.html')
-
-
+    quartos = Quartos.objects.all()
+    contexto={
+        'quart': quartos
+    }
+    return render(request, 'quartos/quartos.html', contexto)
 
 
 def cad_quartos(request):
@@ -19,14 +20,26 @@ def cad_quartos(request):
         descricao = request.POST.get('descricao').strip()
         ativo = request.POST.get('ativo')
         if nome == "" or numero == "" or valor_diaria =="" or capacidade =="" or descricao == "":
-                messages.error(request, "Nome ou conteudo inválidos!")
-                return render(request, 'produtos/quartos.html')
+            messages.error(request, "Nome ou conteudo inválidos!")
+            return render(request, 'quartos/cad_quartos.html')
+        if not nome or not numero or not valor_diaria or not capacidade or not descricao:
+            messages.error(request, "Todos os campos devem ser preenchidos.")
+            return render(request, 'quartos/cad_quartos.html')
+        if float(valor_diaria) <= 0:
+            messages.error(request, "O preço deve ser maior que zero")
+            return render(request, 'produtos/quartos.html')
+        if int(numero)<= 0 or int(capacidade) <= 0:
+            messages.error(request, "O numero deve ser maior que zero")
+            return render(request, 'quartos/cad_quartos.html')
+        if len(nome) < 7:
+            messages.error(request, "nome muito curto!")
+            return render(request, 'quartos/cad_quartos.html')
         else:
             novo_quarto = Quartos()
             novo_quarto.nome = nome
-            novo_quarto.numero = numero
+            novo_quarto.numero = int(numero)
             novo_quarto.valor_diaria = float(valor_diaria)
-            novo_quarto.capacidade = capacidade
+            novo_quarto.capacidade = int(capacidade)
             novo_quarto.descricao = descricao
             novo_quarto.ativo = bool(ativo) # Convertendo para booleano
 
@@ -34,3 +47,12 @@ def cad_quartos(request):
             messages.success(request, "Quarto salvo com sucesso!")
             return redirect('quartos')
     return render(request, 'quartos/cad_quartos.html')
+
+
+
+def detalhes_quartos(request, id_quarto):
+    quartos = get_object_or_404(Quartos, id=id_quarto)
+    contexto={
+        'quart': quartos
+    }
+    return render(request, 'quartos/detalhes_quartos.html', contexto)
--- reservas/admin.py
+++ reservas/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Quartos, Hospedes
+
+class HospedesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'email','data_nasci','cpf', 'telefone', 'cidade','estado']
+    list_display_links=['id','nome','email'  ]
+    list_per_page = 5
+class QuartosAdmin(admin.ModelAdmin):
+    list_display=['id', 'nome', 'numero','valor_diaria','capacidade','descricao','ativo']
+    list_display_links = ['id', 'nome', 'valor_diaria','capacidade']
+    list_per_page = 5
+
 
 # Register your models here.
+admin.site.register(Quartos, QuartosAdmin)
+admin.site.register(Hospedes, HospedesAdmin)
